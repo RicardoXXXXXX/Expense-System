e393eabda5325958d716344494e5f8c26a77f050
Ricardo Xiao <247926625rx@gmail.com>
2023-08-02 09:23:48 +1000

    Add onChange functions

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,60 @@
 import "./ExpenseForm.css";
+import React, { useState } from "react";
 
 const ExpenseForm = () => {
+  //  Example of using one state to manage inputs
+  // const [userInput, setUserInput] = useState({
+  //   inputTitle: "",
+  //   inputAmount: "",
+  //   inputDate: "",
+  // });
+
+  // // titleChangeHandler automatically receive the input as "event" atttribute
+  // const titleChangeHandler = (event) => {
+  //   setUserInput((prevState) => {
+  //     return { ...prevState, inputTitle: event.target.value }; //pull out all input, only change the amount, return updated state
+  //   });
+  // };
+  // const amountChangeHandler = (event) => {
+  //   setUserInput((prevState) => {
+  //     return { ...prevState, inputAmount: event.target.value }; //pull out all input, only change the amount, return updated state
+  //   });
+  // };
+
+  //Use multi-state is easier
+  const [userInputTitle, setTitle] = useState("");
+  const [userInputAmount, setAmount] = useState("");
+  const [userInputDate, setDate] = useState("");
+
+  //event automatically receives the input
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setAmount(event.target.value);
+  };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0" />
+          <input type="number" min="0" onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2023-07-30" max="2100-01-01" />
+          <input
+            type="date"
+            min="2023-07-30"
+            max="2100-01-01"
+            onChange={(event) => {
+              setDate(event.target.value);
+            }}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
